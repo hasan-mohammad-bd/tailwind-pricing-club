5721a8350e2f8556571f74c5d6f4885652e185fe
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-28 10:53:41 +0600

    adding tailwind

---
+++ src/note.js
@@ -0,0 +1,15 @@
+/* 
+install tailwind 
+=========================
+1. commend line: npm install -D tailwindcss postcss autoprefixer
+
+2. then again in commend line: npx tailwindcss init -p
+
+3. replace the content of tailwind.exports.js: 
+
+4. //pease in css :
+@tailwind base;
+@tailwind components;
+@tailwind utilities; 
+
+*/
